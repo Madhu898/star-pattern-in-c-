66c4f16616f7b47f7194fa3473b1774a1f445094
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-11-19 17:39:51 +0530

    Create inverted pyramid.cpp

---
+++ inverted pyramid.cpp
@@ -0,0 +1,32 @@
+/*
+               input  =  5  
+               output = 
+                          * * * * * 
+                           * * * *  
+                            * * *   
+                             * *    
+                              *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    system("cls");
+    int  n;
+    cin>>n;
+    for(int i=1;i<=n;i++ )
+    {
+        for(int k=1;k<i;k++)
+        {
+            cout<<" ";
+        }
+        for(int j=n;j>=i;j--)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
